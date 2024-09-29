18b7f993109457d01fd874d1f5183dbc707ed764
阿菜 Cai <jimmyrss1102@gmail.com>
2024-09-30 03:27:33 +0800

    v2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,9 +11,12 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .menu(capabilities::windows_menu::window_menu())
-        .on_menu_event(|event: tauri::WindowMenuEvent| {
-            capabilities::windows_menu::handle_menu_event(event);
+        .setup(|app| {
+            app.set_menu(capabilities::windows_menu::window_menu())?;
+            app.on_menu_event(move |app, event| {
+                capabilities::windows_menu::handle_menu_event(event);
+            });
+            Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())

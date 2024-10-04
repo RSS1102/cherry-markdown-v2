434e102435ba246e0edcb28d5067f78f8dd4766c
阿菜 Cai <jimmyrss1102@gmail.com>
2024-10-05 00:36:10 +0800

    传递事件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { cherryInstance } from './components/CherryMarkdown';
+import { listen } from "@tauri-apps/api/event";
 
+type File = string
 const App = () => {
   const cherryMarkdown = cherryInstance();
 
-  cherryMarkdown.setMarkdown(`+ Hello, CherryMarkdown!`); 
+  listen<File>('open_file', (event) => cherryMarkdown.setMarkdown(event.payload));
 
 }
 

e14df7cce9dd96e3fd26222d246b47cf5b439083
阿Cai <jimmyrss1102@gmail.com>
2024-10-09 15:15:01 +0800

    尝试动态menu

--- src-tauri/src/implement/system_tray.rs
+++ src-tauri/src/implement/system_tray.rs
@@ -16,33 +16,50 @@ pub fn system_tray_menu(app: &mut App) -> Result<(), tauri::Error> {
     )?;
     let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
 
-    let menu = Menu::with_items(app, &[&show_main_window, &quit])?;
+    // 切换语言
+    let lang = MenuItem::with_id(app, "lang", "切换语言", true, None::<&str>)?;
 
-    let _ = TrayIconBuilder::new()
-        .on_tray_icon_event(|tray, event| match event {
-            TrayIconEvent::Click {
-                button: MouseButton::Left,
-                button_state: MouseButtonState::Up,
-                ..
-            } => {
-                let app = tray.app_handle();
-                restore_and_focus_window(app, "main");
-            }
-            _ => {}
-        })
+    let menu = Menu::with_items(app, &[&show_main_window, &lang, &quit])?;
+
+    let new_menu = Menu::with_items(app, &[&show_main_window])?;
+
+    let tray_menu = TrayIconBuilder::with_id("tray")
         .menu(&menu)
-        .on_menu_event(|app, event| match event.id.as_ref() {
-            "show_main_window" => {
-                restore_and_focus_window(app, "main");
-            }
-            "quit" => {
-                app.exit(0);
-            }
-            _ => {}
-        })
         .menu_on_left_click(false)
         .icon(app.default_window_icon().unwrap().clone())
         .build(app)?;
+
+    tray_menu.on_tray_icon_event(|tray, event| match event {
+        TrayIconEvent::Click {
+            button: MouseButton::Left,
+            button_state: MouseButtonState::Up,
+            ..
+        } => {
+            let app = tray.app_handle();
+            restore_and_focus_window(app, "main");
+        }
+        _ => {}
+    });
+
+    let tray_menu_clone = tray_menu.clone();
+
+    tray_menu.on_menu_event(move |app, event| match event.id.as_ref() {
+        "show_main_window" => {
+            restore_and_focus_window(app, "main");
+        }
+        "lang" => {
+            match tray_menu_clone.set_menu(Some(new_menu.clone())) {
+                Ok(_) => {}
+                Err(e) => {
+                    eprintln!("Error setting menu: {:?}", e);
+                }
+            }
+        }
+        "quit" => {
+            app.exit(0);
+        }
+        _ => {}
+    });
 
     Ok(())
 }
